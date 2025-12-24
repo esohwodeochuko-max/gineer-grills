dc2e7ab5c74c021164aae80aa6285d4ed11ce6de
esohwodeochuko-max <esohwodeochuko@gmail.com>
2025-12-24 22:12:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './index.css';
 import React, { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, onSnapshot, serverTimestamp, deleteDoc, doc, query, orderBy } from 'firebase/firestore';
